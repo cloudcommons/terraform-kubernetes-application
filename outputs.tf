76cc5548793ff6f047710d155a81aaeb5b1410ad
Sergio Sisternes <sesispla@outlook.com>
2019-12-29 14:10:55 +0100

    Added some output variables

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "namespace" {
+  description = "Namespace created for this application"
+  value       = local.namespace
+}
+
+output "deployments" {
+  description = "List of deployment names created"
+  value       = kubernetes_deployment.cloudcommons[*].metadata[0].name
+}
+
+output "services" {
+  description = "List of service names created"
+  value       = kubernetes_service.cloudcommons[*].metadata[0].name
+}
+
+output "ingress" {
+  description = "Ingress name creates"
+  value       = kubernetes_ingress.cloudcommons.metadata[0].name
+}
