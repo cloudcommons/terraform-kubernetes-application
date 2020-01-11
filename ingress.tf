d857b6328f169125a724ff8341c5edb98f5b4f57
Sergio Sisternes <sesispla@outlook.com>
2020-01-11 01:03:57 +0100

    Fixed error when no versions are specified

--- ingress.tf
+++ ingress.tf
@@ -8,7 +8,7 @@ resource "kubernetes_ingress" "cloudcommons" {
 
   spec {
     dynamic "backend" {
-      for_each = var.SERVICE_ENABLED == true && var.INGRESS_DEFAULT_BACKEND_ENABLED == true ? [1] : [] # Default back-end
+      for_each = length(var.VERSIONS) > 0 && var.SERVICE_ENABLED == true && var.INGRESS_DEFAULT_BACKEND_ENABLED == true ? [1] : [] # Default back-end
       content {
         service_name = kubernetes_service.cloudcommons[backend.key].metadata[0].name
         service_port = kubernetes_service.cloudcommons[backend.key].spec[0].port[0].port
